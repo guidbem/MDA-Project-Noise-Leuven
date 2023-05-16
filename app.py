d7e67927d451ca81d04059cb54fb3c1d2b41b409
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-05-16 22:16:10 +0200

    navbar

--- app.py
+++ app.py
@@ -1,24 +1,56 @@
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
 import dash
+import dash_vtk
 
-app = Dash(__name__, use_pages=True)
 
-app.layout = html.Div([
-	html.H1('Multi-page app with Dash Pages'),
+app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
 
-    html.Div(
-        [
-            html.Div(
-                dcc.Link(
-                    f"{page['name']} - {page['path']}", href=page["relative_path"]
-                )
-            )
-            for page in dash.page_registry.values()
-        ]
-    ),
+nav_bar = html.Nav(
+    children=[
+        dcc.Link(
+            "Overview",
+            href="/",
+            className="link",  # Added "link" class
+        ),
+        dcc.Link(
+            "Analysis 1",
+            href="/analysis-1",
+            className="link",  # Added "link" class
+        ),
+        dcc.Link(
+            "Analysis 2",
+            href="/analysis_2",
+            className="link",  # Added "link" class
+        ),
+        dcc.Link(
+            "Model",
+            href="/model",
+            className="link",  # Added "link" class
+        ),
+    ],
+    style={
+        "background-color": "white",
+        "height": "70px",
+        "width": "35%",
+        "display": "flex",
+        "align-items": "center",
+        "margin-left": "auto",
+        "margin-right": "auto",
+    },
+)
 
-	dash.page_container
-])
+app.layout = html.Div(
+    [   
+        html.Link(
+            rel="stylesheet",
+            href="/assets/styles.css"  # Path to the CSS file
+        ),
+        nav_bar,
+        dash.page_container
+    ]
+)
+
 
 if __name__ == '__main__':
-	app.run_server(debug=True)
+    app.run_server(debug=True)
