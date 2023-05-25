f83a0de6b05fccacf4535e604395cc339de087d5
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-05-25 16:44:57 +0200

    Images analysis 2

--- APP/pages/analysis2.py
+++ APP/pages/analysis2.py
@@ -8,6 +8,12 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/analysis-2')
 
-layout=html.Div([
-    html.H1('Analysis 2')
+layout = html.Div([
+    html.H1('Analysis 2'),
+    html.Div([
+        html.Img(src="assets/acf_minutes_noise.png", style={'width': '33%'}),
+        html.Img(src="assets/diff1_laeq.png", style={'width': '33%'}),
+                html.Img(src="assets/diff1_144.png", style={'width': '33%'}),
+    ], style={'display': 'flex'}),
+    html.Div(html.Img(src="assets/Meteo_PCA.png", style={'display': 'block', 'margin': 'auto'}))
 ])
