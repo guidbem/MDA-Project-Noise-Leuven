a5338c7cde4047a7d89c280954435c9fd03ec242
Federicosoldati <federico.soldati@student.kuleuven.be>
2023-05-30 17:52:14 +0200

    Fixed nav bar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Output, Input,callback_context  
 import dash_bootstrap_components as dbc
 import dash
 
@@ -11,29 +11,34 @@ nav_bar = html.Nav(
             href="/",
             className="link",
             style={"margin-right": "20px"},
+            id="overview-link",
         ),
         dcc.Link(
             "Analysis 1",
             href="/analysis-1",
             className="link",
             style={"margin-right": "20px"},
+            id="analysis-1-link",
         ),
         dcc.Link(
             "Analysis 2",
             href="/analysis-2",
             className="link",
             style={"margin-right": "20px"},
+            id="analysis-2-link",
         ),
         dcc.Link(
             "Model",
             href="/model",
             className="link",
             style={"margin-right": "0px"},
+            id="model-link",
         ),
         dcc.Link(
             "About Us",
             href="/about_us",
             className="link",
+            id="about-us-link",
         ),
     ],
     style={
@@ -56,10 +61,19 @@ app.layout = html.Div(
             rel="stylesheet",
             href="/assets/styles.css"  # Path to the CSS file
         ),
+        dcc.Location(id="url", refresh=False),
         nav_bar,
         dash.page_container
     ]
 )
+
+
+@app.callback(
+    [dash.dependencies.Output(f"{link_id}", "className") for link_id in ["overview-link", "analysis-1-link", "analysis-2-link", "model-link", "about-us-link"]],
+    [dash.dependencies.Input("url", "pathname")]
+)
+def update_active_link(pathname):
+    return ["link active" if f"/{link_id.replace('-link', '')}" == pathname or link_id == "overview-link" and pathname == "/" else "link" for link_id in ["overview-link", "analysis-1-link", "analysis-2-link", "model-link", "about_us-link"]]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
